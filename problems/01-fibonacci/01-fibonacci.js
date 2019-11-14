83e7b67302ae2f6ccd1f11529b8e73e904a52f6e
Matthew <msbeeman@github.com>
2019-11-14 10:12:43 -0500

    01-fibonacci done

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,24 @@
 /* 1. Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
-function fib(n) {
+function fib(n){
+    //Type Checking
+    if(isNaN(n)){
+        throw "This is not a valid number"
+    }
+    if(n < 0){
+        throw new Error('n must be greater than 0')
+    }
+    if(n === Infinity){
+        throw "n cannot be infinity"
+    }
 
+    //Base Cases
+    if(n === 0){
+        return 0
+    }
+    if(n === 1){
+        return 1
+    }
+    return fib(n-1) + fib(n-2)
 }
