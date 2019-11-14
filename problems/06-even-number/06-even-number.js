385ade41b6f89df8865d3d0de7d260ccc3741884
Matthew <msbeeman@github.com>
2019-11-13 19:10:26 -0500

    06-even-number finished

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -2,6 +2,17 @@
 Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
+
 function isEven(someNum) {
-  
+    
+    let num = someNum
+
+    if(Number.isInteger(num / 2)){
+        return true
+    } else {
+        return false
+    }
+
 }
+
+console.log(isEven(2));
