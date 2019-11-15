ec620fc1f1705bcc682172de6fd9e31b29bbbf7b
Matthew <msbeeman@github.com>
2019-11-15 17:42:41 -0500

    10-object-creation complete.

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,38 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+//Object Literal
+const car = {
+    make: 'Acura',
+    model: 'Integra',
+    age: 24,
+    horsePower: 140,
+    torque: 128
+}
+
+//Constructor Function
+function car(make, model, age, horsePower, torque){
+    this.make = make,
+    this.model = model,
+    this.age = age,
+    this.horsePower = horsePower,
+    this.torque = torque,
+    name = make + model
+}
+
+//ES6 CLass
+class car{
+    make
+    model
+    age
+    horsePower
+    torque
+    constructor(make, model, age, horsePower, torque){
+        this.make = make,
+        this.model = model,
+        this.age = age,
+        this.horsePower = horsePower,
+        this.torque = torque
+    }
+}
