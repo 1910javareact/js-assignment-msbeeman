1ad76a3998b7d8d8730c19ac6aa432ea8565c2c2
Matthew <msbeeman@github.com>
2019-11-15 12:20:27 -0500

    testing skeleton

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -1,6 +1,22 @@
 /* 9. Object Properties
 Define function objectProperties(someObj)
 Print every property and it's value. */
+someObj = {
+    dataType: "Object",
+    name: "someObj",
+    date: {
+        dayCreated: 15,
+        monthCreated: "november",
+        yearCreated: 2019
+    }
+}
+
+
 function objectProperties(someObj) {
 
+
 }
+
+
+console.log(objectProperties(someObj));
+
