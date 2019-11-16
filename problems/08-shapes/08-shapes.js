cdd102270afb51c15c9a7a38e751cc6af5407351
Matthew <msbeeman@github.com>
2019-11-15 20:28:42 -0500

    progress save

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -74,7 +74,38 @@ function printShape(shape, height, character) {
     }
     case "diamond":
     case "Diamond": {
-      break;
+
+      let emptyStr = ""
+      let charSliceCounter = 1;
+      let emptySpaceCounter = Math.ceil(height/2);
+
+      //Create the row of empty space 
+      for(let i = 0; i < height; i++){
+        emptyStr = emptyStr + " "  
+      }
+
+      //Create the row of characters, length being height
+      for(let i = 0; i < height; i++){
+        charArray = charArray + character    
+      }
+
+      //Iterate and create the top half of the diamond
+      for(let j = 0; j < Math.ceil(height/2); j++){
+        console.log(emptyStr.slice(0, emptySpaceCounter) + charArray.slice(0, charSliceCounter));
+        charSliceCounter += 2
+        emptySpaceCounter --
+      }
+
+      //Iterate and create the bottom half of the diamond
+      charSliceCounter = charSliceCounter - 2
+      emptySpaceCounter = emptySpaceCounter + 1
+      for(let i = Math.ceil(height/2); i < height; i++){
+          charSliceCounter -= 2
+          emptySpaceCounter ++
+          console.log(emptyStr.slice(0, emptySpaceCounter) + charArray.slice(0, charSliceCounter));
+      }   
+
+      return ""
     }
   }
 }
@@ -82,6 +113,8 @@ function printShape(shape, height, character) {
 
 console.log(printShape("Square", 3, "%"));
 console.log(printShape("Triangle", 3, "$"));
+console.log(printShape("Diamond", 5, "*"));
+
 
 
 
